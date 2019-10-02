0e26f4a5a3325a065f68782ceef82df4d96aedd1
RENATU BONFIM <renatu_mb@yahoo.com.br>
2019-10-02 14:45:06 -0700

    https://leetcode.com/problems/rotate-array

--- src/main/java/RotateArray.java
+++ src/main/java/RotateArray.java
@@ -5,8 +5,8 @@ public class RotateArray {
     public static void main(String[] args) {
 
 
-        //rotate( new  int [] {1,2,3,4,5,6,7},7 ) ;
-        rotate(new int[]{1, 2}, 3);
+        rotate2(new int[]{1, 2, 3, 4, 5, 6, 7}, 3);
+        rotate2(new int[]{1, 2}, 3);
     }
 
 
@@ -25,6 +25,28 @@ public class RotateArray {
             insert = insert == nums.length ? 0 : insert;
         }
         System.out.println(Arrays.toString(nums));
+    }
+
+
+    public static void rotate2(int[] nums, int k) {
+
+        k = k % nums.length;
+        reverseArray(nums, 0, nums.length - 1);
+        reverseArray(nums, 0, k - 1);
+        reverseArray(nums, k, nums.length - 1);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    private static void reverseArray(int[] nums, int ini, int end) {
+        while (ini < end) {
+
+            int t = nums[ini];
+            nums[ini] = nums[end];
+            nums[end] = t;
+
+            ini++;
+            end--;
+        }
     }
 }
 
